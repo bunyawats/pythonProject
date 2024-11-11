a7758c1109fca62c391072afca832948380aed53
Bunyawat Singchai <bunyawat.s@gmail.com>
2024-11-11 09:35:59 +0700

    Add LANGCHAIN_API_KEY loading

---
+++ sandbox/rag_main_a.py
@@ -0,0 +1,48 @@
+from langchain_community.document_loaders import PDFPlumberLoader
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_ollama import OllamaLLM
+from langchain import hub
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+
+
+
+# Load the PDF
+loader = PDFPlumberLoader("11pests1disease.pdf")
+docs = loader.load()
+
+# Split into chunks
+text_splitter = SemanticChunker(HuggingFaceEmbeddings())
+documents = text_splitter.split_documents(docs)
+
+# Instantiate the embedding model
+embedder = HuggingFaceEmbeddings()
+
+# Create the vector store and fill it with embeddings
+vector = FAISS.from_documents(documents, embedder)
+retriever = vector.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+
+# Define llm
+llm = OllamaLLM(model="llama3")
+
+# Prompt
+prompt = hub.pull("rlm/rag-prompt")
+
+# Post-processing
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+# Chain
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+# Question
+ans = rag_chain.invoke("How does plant respond to disease?")
+print(ans)
