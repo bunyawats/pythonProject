acc5a800648a1c39d922fefa51651f6d33fd2c3c
Bunyawat Singchai <bunyawat.s@gmail.com>
2024-11-30 20:27:18 +0700

    call external functions using LCEL

--- sandbox/tool_call_test.py
+++ sandbox/tool_call_test.py
@@ -1,13 +1,12 @@
 from langchain_core.messages import HumanMessage
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnableLambda
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from langchain_core.tools import tool
 import os
 from dotenv import load_dotenv
 
 from langchain_core.tools import tool
 from langchain_ollama import ChatOllama
-
 
 pathDir = os.path.join(os.getcwd(), ".env")
 # print(pathDir)
@@ -17,6 +16,7 @@ llm = ChatOllama(
     model="llama3.1",
     temperature=0,
 )
+
 
 @tool
 def add(a: int, b: int) -> int:
@@ -29,38 +29,30 @@ def multiply(a: int, b: int) -> int:
     """Multiplies a and b."""
     return a * b
 
-
-def composToolCallOutput( ai_msg ) :
-    print(ai_msg.tool_calls)
-
+def compose_tool_call_output(input: dict):
+    print(input["ai_msg"].tool_calls)
     print(">>" * 100)
-
-    messages.append(ai_msg)
-
-    for tool_call in ai_msg.tool_calls:
+    input["messages"].append(input["ai_msg"])
+    for tool_call in input["ai_msg"].tool_calls:
         selected_tool = {"add": add, "multiply": multiply}[tool_call["name"].lower()]
         tool_msg = selected_tool.invoke(tool_call)
-        messages.append(tool_msg)
+        input["messages"].append(tool_msg)
 
-    return messages
-
+    return input["messages"]
 
 tools = [add, multiply]
-
 llm_with_tools = llm.bind_tools(tools)
 
+chain = (RunnableLambda(lambda x: [HumanMessage(x)])
+         | {"ai_msg": RunnablePassthrough() | llm_with_tools, "messages": RunnablePassthrough()}
+         | RunnableLambda(compose_tool_call_output)
+         | llm_with_tools
+         | StrOutputParser())
+
 query = "What is 3 * 12? Also, what is 11 + 49?"
-messages = [HumanMessage(query)]
-# ai_msg = llm_with_tools.invoke(messages)
-
-# messages = composToolCallOutput(ai_msg)
-
-chain = llm_with_tools | RunnableLambda(composToolCallOutput) | llm_with_tools | StrOutputParser()
-print( chain.invoke(messages))
+print(chain.invoke(query))
 
 # print( StrOutputParser().invoke( llm_with_tools.invoke(messages) ))
-
-
 
 
 # tool_chain = llm_with_tools | RunnableLambda(composToolCallOutput) | llm_with_tools | StrOutputParser
