f6fcf4cc7e6efd1bbf633f6ba93a4b03c22838fb
Bunyawat Singchai <bunyawat.s@gmail.com>
2024-12-04 18:52:10 +0700

    create rest api to call myhr service api "boss_detail"

--- app/routes/my_hr_route.py
+++ app/routes/my_hr_route.py
@@ -1,14 +1,12 @@
 import json
 
 from fastapi import APIRouter, Path, Body, Header, HTTPException
+
+from app.models.query_model import QueryModel
 from sandbox.call_rest_service import call_boss_detail
 from sandbox.tool_call_test import query_llm
 
 router = APIRouter()
-
-
-# company_id = "eb2f4f30-edaf-11ee-a69a-c7680edc0e47"
-# user_token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzY2hlbWEiOiJkYm8iLCJlbmNvZGUiOiIyIiwic3ViIjoiQXV0aCIsImNvbXBhbnlOYW1lIjoi4Lia4Lij4Li04Lip4Lix4LiXIOC4oeC4suC4ouC5gOC4reC4iuC4reC4suC4o-C5jCDguIjguLPguIHguLHguJQiLCJkYk5hbWUiOiJNWUhSUExVUyIsInJvbGVzIjpbIlVTRVIiXSwid29ya2FyZWEiOiJUS1ciLCJpc3MiOiJDb21wdXRlciBTY2llbmNlIENvcnBvcmF0aW9uIExpbWl0ZWQiLCJ6bWxvZ2luIjoiZmFsc2UiLCJyb2xlX2xldmVsIjoiNiIsImVtcGxveWVlaWQiOiIxMDAwMDA4MiIsImJyYW5jaCI6Im15aHIiLCJlbXBfcG9zaXRpb24iOiIwOTciLCJ1c2VyX3JvbGUiOiJBbGwiLCJ1aWQiOiIxMDAwMDA4MiIsImNvbXBhbnlpZCI6IjEwMCIsImFjdG9yaWQiOiIxMDAwMDA4MiIsImxhbmciOiJ0aCIsImFkIjoiZmFsc2UiLCJmaXJzdGxvZ2luIjoiZmFsc2UiLCJ1cmxfbXlociI6Imh0dHA6Ly9ocnBsdXMtc3RkLm15aHIuY28udGgvaHIiLCJhcHBfbmFtZSI6Im15aHIiLCJyZWdpb25hbGx0eSI6IkVORyIsInRva2VuX3plZW1lIjoiIiwidXNlcl9sZXZlbCI6Ik1ZSFIiLCJmdWxsbmFtZSI6IuC4meC4suC4ouC4reC4nuC4tOC4o-C4seC4leC4meC5jCAg4LiX4LiU4Liq4Lit4LiaIiwiY29taWQiOiIiLCJqb2IiOiIwOTctMjQ2OSIsInVzZXIiOiJteWhyIiwiem1fdXNlciI6IiIsInVzZXJuYW1lIjoibXlociIsIm1lbWJlcmlkIjoiIn0.R70ZQ1_HPA1pq-jeyxD-K4eKZKLYVIg2jmFDhenQjQc"
 
 @router.get("/boss_detail/{company_id}")
 async def boss_detail(
@@ -26,8 +24,9 @@ async def boss_detail(
     json_string =  call_boss_detail(company_id, user_token)
     return json.loads(json_string)
 
-@router.get("/chat_query/{company_id}")
+@router.post("/chat_query/{company_id}")
 async def chat_query(
+        query: QueryModel = Body(...),
         company_id: str = Path(..., title="The ID of the item to create"),
         authorization: str = Header(...),
 ):
@@ -39,7 +38,6 @@ async def chat_query(
     user_token = authorization[len("Bearer "):]
     # print(user_token)
 
-    query = "Who is my boss?"
-    answer =  query_llm(query, company_id, user_token)
+    answer =  query_llm(query.message, company_id, user_token)
     return answer
 
--- sandbox/tool_call_test.py
+++ sandbox/tool_call_test.py
@@ -6,11 +6,14 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from langchain_core.tools import tool
 from langchain_ollama import ChatOllama
-from sympy.polys.polyconfig import query
+
+from sandbox.call_rest_service import call_boss_detail
 
 pathDir = os.path.join(os.getcwd(), ".env")
-# print(pathDir)
 load_dotenv(pathDir)
+
+global_company_id = ""
+global_user_token = ""
 
 def compose_tool_call_output(input: dict):
     print(input["ai_msg"].tool_calls)
@@ -51,8 +54,10 @@ def get_boss_detail() -> str:
     :return: Boss Name
     """
 
-    return "Bunyawat Singchai"
+    company_id = "eb2f4f30-edaf-11ee-a69a-c7680edc0e47"
+    user_token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzY2hlbWEiOiJkYm8iLCJlbmNvZGUiOiIyIiwic3ViIjoiQXV0aCIsImNvbXBhbnlOYW1lIjoi4Lia4Lij4Li04Lip4Lix4LiXIOC4oeC4suC4ouC5gOC4reC4iuC4reC4suC4o-C5jCDguIjguLPguIHguLHguJQiLCJkYk5hbWUiOiJNWUhSUExVUyIsInJvbGVzIjpbIlVTRVIiXSwid29ya2FyZWEiOiJUS1ciLCJpc3MiOiJDb21wdXRlciBTY2llbmNlIENvcnBvcmF0aW9uIExpbWl0ZWQiLCJ6bWxvZ2luIjoiZmFsc2UiLCJyb2xlX2xldmVsIjoiNiIsImVtcGxveWVlaWQiOiIxMDAwMDA4MiIsImJyYW5jaCI6Im15aHIiLCJlbXBfcG9zaXRpb24iOiIwOTciLCJ1c2VyX3JvbGUiOiJBbGwiLCJ1aWQiOiIxMDAwMDA4MiIsImNvbXBhbnlpZCI6IjEwMCIsImFjdG9yaWQiOiIxMDAwMDA4MiIsImxhbmciOiJ0aCIsImFkIjoiZmFsc2UiLCJmaXJzdGxvZ2luIjoiZmFsc2UiLCJ1cmxfbXlociI6Imh0dHA6Ly9ocnBsdXMtc3RkLm15aHIuY28udGgvaHIiLCJhcHBfbmFtZSI6Im15aHIiLCJyZWdpb25hbGx0eSI6IkVORyIsInRva2VuX3plZW1lIjoiIiwidXNlcl9sZXZlbCI6Ik1ZSFIiLCJmdWxsbmFtZSI6IuC4meC4suC4ouC4reC4nuC4tOC4o-C4seC4leC4meC5jCAg4LiX4LiU4Liq4Lit4LiaIiwiY29taWQiOiIiLCJqb2IiOiIwOTctMjQ2OSIsInVzZXIiOiJteWhyIiwiem1fdXNlciI6IiIsInVzZXJuYW1lIjoibXlociIsIm1lbWJlcmlkIjoiIn0.R70ZQ1_HPA1pq-jeyxD-K4eKZKLYVIg2jmFDhenQjQc"
 
+    return call_boss_detail(company_id, user_token)
 
 def query_llm(chat_query : str, company_id : str, user_token: str) -> str:
 
